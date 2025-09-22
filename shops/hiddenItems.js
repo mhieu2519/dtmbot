6e547be3035cff2c72123aa14ff3b1c15568d82b
mhieu2519 <nguyenminhhieu2000hd@gmail.com>
2025-09-22 10:46:05 +0700

    teesstAncient4

--- shops/hiddenItems.js
+++ shops/hiddenItems.js
@@ -171,7 +171,7 @@ module.exports = [
     description: "Xuất hiện vào thiên cổ thời đại, chứa đựng sức mạnh vô hạn ( mở khóa di tích cổ).",
     rarity: "epic",
     quantity: 1,
-    weight: 1000
+    weight: 15
   },
 
 ];
